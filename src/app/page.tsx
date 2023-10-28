927a19d539f73050b1d0f9ed320b78eedd34a6a9
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2023-10-29 00:08:33 +0500

    barebones design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,224 @@
-import Image from 'next/image'
+"use client";
+import { useEffect, useState } from "react";
+import MathJax from "react-mathjax2";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [numberOfInputs, setNumberOfInputs] = useState(1);
+	const [roots, setRoots] = useState<number[]>([]);
+	const [inputs, setInputs] = useState<number[]>([]);
+	const [selectedMethod, setSelectedMethod] = useState("Fixed Point");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [equation, setEquation] = useState("");
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const listOfVarriableNames = [
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	];
+	const listOfMethods = [
+		"Fixed Point",
+		"Bisection",
+		"Newton Raphson",
+		"Secant",
+	];
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	useEffect(() => {
+		if (numberOfInputs < 1) {
+			setNumberOfInputs(1);
+		}
+		if (numberOfInputs > 26) {
+			setNumberOfInputs(26);
+		}
+	}, [numberOfInputs]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	useEffect(() => {
+		let eq = "";
+		for (let i = 0; i < numberOfInputs; i++) {
+			if (inputs[i] && inputs[i] != 0) {
+				eq += `${inputs[i]}`;
+				if (i !== numberOfInputs - 1) {
+					eq += `${listOfVarriableNames[i]} ^ ${
+						numberOfInputs - i - 1
+					} + `;
+				}
+			}
+		}
+		eq += " = 0";
+		setEquation(eq);
+		if (selectedMethod === "Fixed Point") {
+			FixedPoint();
+		}
+	}, [numberOfInputs, roots, inputs, listOfVarriableNames, selectedMethod]);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	const FixedPoint = () => {
+		let startingPoint = 0;
+		let maxIterations = 100;
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		for (let i = 0; i < maxIterations; i++) {
+			let result = 0;
+			for (let j = 0; j < numberOfInputs; j++) {
+				result +=
+					inputs[j] * Math.pow(startingPoint, numberOfInputs - j - 1);
+			}
+			if (result === 0) {
+				setRoots((prev) => [...prev, startingPoint]);
+				break;
+			}
+			startingPoint = result;
+		}
+	};
+	return (
+		<MathJax.Context input="ascii">
+			<main className="flex min-h-screen flex-col items-center p-24 gap-12">
+				<header className="flex flex-col items-center">
+					<h1 className="text-2xl font-bold">
+						Numerical Analysis Calculator
+					</h1>
+					<p className="text-sm">
+						A simple calculator for numerical analysis.
+					</p>
+				</header>
+				<section className="flex items-center justify-center gap-4">
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="method"
+								className="mr-2 block text-lg font-medium">
+								Method
+							</label>
+							<select
+								id="method"
+								className="block w-40 rounded-md text-black py-2 px-3"
+								value={selectedMethod}
+								onChange={(e) =>
+									setSelectedMethod(e.target.value)
+								}>
+								{listOfMethods.map((method) => (
+									<option
+										key={method}
+										value={method}>
+										{method}
+									</option>
+								))}
+							</select>
+						</div>
+					</div>
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="numberOfInputs"
+								className="mr-2 block text-lg font-medium">
+								Number of Inputs:
+							</label>
+							<input
+								type="number"
+								id="numberOfInputs"
+								className="block w-20 rounded-md text-black py-2 px-3"
+								value={numberOfInputs}
+								onChange={(e) =>
+									setNumberOfInputs(parseInt(e.target.value))
+								}
+							/>
+						</div>
+					</div>
+				</section>
+				<section className="flex items-center justify-center">
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="numberOfInputs"
+								className="mr-2 block text-lg font-medium">
+								Constants:
+							</label>
+							<section className="flex items-center justify-center flex-wrap">
+								{numberOfInputs &&
+									Array(numberOfInputs)
+										.fill(0)
+										.map((_, index) => (
+											<div
+												key={index}
+												className="mx-2">
+												<label
+													htmlFor="constant"
+													className="ml-2 block text-lg font-medium">
+													{
+														listOfVarriableNames[
+															index
+														]
+													}
+												</label>
+												<input
+													type="number"
+													id="constant"
+													className="block w-20 rounded-md text-black py-2 px-3 my-2"
+													value={inputs[index]}
+													onChange={(e) => {
+														const newInputs = [
+															...inputs,
+														];
+														newInputs[index] =
+															parseFloat(
+																e.target.value
+															);
+														setInputs(newInputs);
+													}}
+												/>
+											</div>
+										))}
+							</section>
+						</div>
+					</div>
+				</section>
+				{/* Equation */}
+				<section className="flex items-center justify-center">
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="equation"
+								className="mr-2 block text-lg font-medium">
+								Equation:
+							</label>
+
+							<MathJax.Node>{equation}</MathJax.Node>
+						</div>
+					</div>
+				</section>
+				<section className="flex items-center justify-center">
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="roots"
+								className="mr-2 block text-lg font-medium">
+								Roots:
+							</label>
+							<MathJax.Node>{roots.join(", ")}</MathJax.Node>
+						</div>
+					</div>
+				</section>
+			</main>
+		</MathJax.Context>
+	);
 }
