1edd98ffc0ba85835ce31b6f60faebd9314e0a17
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2023-11-04 10:58:50 +0500

    added more inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import MathJax from "react-mathjax2";
 
 export default function Home() {
 	const [numberOfInputs, setNumberOfInputs] = useState(1);
+	const [iterations, setIterations] = useState(0);
 	const [roots, setRoots] = useState<number[]>([]);
 	const [inputs, setInputs] = useState<number[]>([]);
 	const [selectedMethod, setSelectedMethod] = useState("Fixed Point");
@@ -55,51 +56,79 @@ export default function Home() {
 	}, [numberOfInputs]);
 
 	useEffect(() => {
-		let eq = "";
+		let eq = "f(x) = ";
 		for (let i = 0; i < numberOfInputs; i++) {
 			if (inputs[i] && inputs[i] != 0) {
 				eq += `${inputs[i]}`;
 				if (i !== numberOfInputs - 1) {
-					eq += `${listOfVarriableNames[i]} ^ ${
+					eq += `${listOfVarriableNames[(i+23)%listOfVarriableNames.length]} ^ ${
 						numberOfInputs - i - 1
 					} + `;
 				}
 			}
 		}
-		eq += " = 0";
+		// eq += " = 0";
 		setEquation(eq);
-		if (selectedMethod === "Fixed Point") {
+		if (selectedMethod === "Fixed Point" && iterations > 0) {
 			FixedPoint();
 		}
-	}, [numberOfInputs, roots, inputs, listOfVarriableNames, selectedMethod]);
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [numberOfInputs, inputs, listOfVarriableNames, selectedMethod]);
+
+const functionValue = (x: number|undefined) => {
+		if (!x){
+			return 0
+		}
+		let sol = 0;
+		for (let i = 0; i < numberOfInputs; i++) {
+			sol += inputs[i] * Math.pow(x, numberOfInputs - i - 1);
+		}
+		return sol;
+	}
 
 	const FixedPoint = () => {
-		let startingPoint = 0;
-		let maxIterations = 100;
-
-		for (let i = 0; i < maxIterations; i++) {
-			let result = 0;
-			for (let j = 0; j < numberOfInputs; j++) {
-				result +=
-					inputs[j] * Math.pow(startingPoint, numberOfInputs - j - 1);
-			}
-			if (result === 0) {
-				setRoots((prev) => [...prev, startingPoint]);
+		setRoots([]);
+		let rangeStart = 0;
+		let rangeEnd = 1;
+		let i = 0;
+		while (functionValue(rangeStart) * functionValue(rangeEnd) >= 0) {
+			    // keep going until range start and end have values with opposite signs
+			    rangeStart -= 1;
+			    rangeEnd += 1;
+				console.log(rangeStart, rangeEnd)
+		}
+		while (iterations && i < iterations) {
+			let root = (rangeStart + rangeEnd) / 2;
+			if (functionValue(root) == 0) {
+				setRoots((prevRoots) => [...prevRoots, root]);
 				break;
+			} else if (functionValue(root) * functionValue(rangeStart) < 0) {
+				rangeEnd = root;
+			} else {
+				rangeStart = root;
 			}
-			startingPoint = result;
+			setRoots((prevRoots) => [...prevRoots, root]);
+			i++;
 		}
 	};
 	return (
 		<MathJax.Context input="ascii">
 			<main className="flex min-h-screen flex-col items-center p-24 gap-12">
-				<header className="flex flex-col items-center">
-					<h1 className="text-2xl font-bold">
-						Numerical Analysis Calculator
-					</h1>
-					<p className="text-sm">
-						A simple calculator for numerical analysis.
-					</p>
+				<header className="flex items-center w-full justify-between">
+					<section className="flex flex-col items-center">
+						<h1 className="text-2xl font-bold">
+							Numerical Analysis Calculator
+						</h1>
+						<p className="text-sm">
+							A simple calculator for numerical analysis.
+						</p>
+					</section>
+					<MathJax.Node>
+						{
+							// "Numer" written as numbers
+							"\\(\\text{Numer} \\)"
+						}
+					</MathJax.Node>
 				</header>
 				<section className="flex items-center justify-center gap-4">
 					<div className="flex flex-col items-center">
@@ -124,6 +153,24 @@ export default function Home() {
 									</option>
 								))}
 							</select>
+						</div>
+					</div>
+					<div className="flex flex-col items-center">
+						<div className="flex justify-center items-center">
+							<label
+								htmlFor="iterations"
+								className="mr-2 block text-lg font-medium">
+								Iterations:
+							</label>
+							<input
+								type="number"
+								id="iterations"
+								className="block w-20 rounded-md text-black py-2 px-3"
+								value={iterations}
+								onChange={(e) =>
+									setIterations(parseInt(e.target.value))
+								}
+							/>
 						</div>
 					</div>
 					<div className="flex flex-col items-center">
